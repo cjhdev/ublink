5617a4bb6ae72d84bf65568b7520af8f3fb6ffe8
cjhdev <cam@cjh.id.au>
2017-01-20 14:17:01 +0000

    generator development

---
+++ app/generate/generate.h
@@ -0,0 +1,11 @@
+#ifndef GENERATE_H
+#define GENERATE_H
+
+#include "blink_schema.h"
+
+struct arguments {
+
+    blink_schema_t schema;
+};
+
+#endif
---
+++ app/generate/generate_c.h
@@ -0,0 +1,16 @@
+#ifndef GENERATE_C
+#define GENERATE_C
+
+/* includes ***********************************************************/
+
+#include <stdbool.h>
+
+/* types **************************************************************/
+
+struct arguments;
+
+/* functions **********************************************************/
+
+bool GenerateC(struct arguments *arg);
+
+#endif
